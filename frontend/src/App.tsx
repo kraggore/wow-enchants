f2d9f4c39ba08c6c736e33b9308dec0c97270875
kraggore <kraggore@crfx.gg>
2025-02-03 23:56:18 +0100

    bububu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo } from 'react'
 import './App.css'
 
-const API_BASE_URL = process.env.DB_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 interface Reagent {
   name: string;
