413ecff9e9600ca1496fa126743c4f155f2477de
kraggore <kraggore@crfx.gg>
2025-02-03 22:46:13 +0100

    spiderman

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_origins=["http://10.0.0.1:5173", "http://localhost:5173", "http://wow-enchants.murumb.dev"],  # Vite's default port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
