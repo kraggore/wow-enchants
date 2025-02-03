c5b1dea31c34e14bdc6c5f7f16a24fdfed68e597
kraggore <kraggore@crfx.gg>
2025-02-03 22:48:59 +0100

    rhino

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['wow-enchants.murumb.dev', '10.0.0.2', 'localhost']
+  }
 })
