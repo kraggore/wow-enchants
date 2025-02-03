f27e1b335e90446058f0e334dd0377108e95a46b
kraggore <kraggore@crfx.gg>
2025-02-03 23:35:47 +0100

    bastapasta

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -5,6 +6,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['wow-enchants.murumb.dev', '10.0.0.2', 'localhost']
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
     allowedHosts: ['wow-enchants.murumb.dev', '10.0.0.2', 'localhost']
   }
 })
